f2adf9c0c2124af002946a6666b25f86b88e7183
Josef Andersson <janderssonse@proton.me>
2024-09-09 22:28:35 +0200

    fix: remove golanglintci cache
    Signed-off-by: Josef Andersson <janderssonse@proton.me>

--- scripts/installdevprereq.sh
+++ scripts/installdevprereq.sh
@@ -39,7 +39,7 @@ NC='\033[0m' # No Color
 # Define Go tools to install
 GO_TOOLS=(
   "github.com/vektra/mockery/v2@v2.44.1"
-  "github.com/golangci/golangci-lint/cmd/golangci-lint@v1.60.2"
+  "github.com/golangci/golangci-lint/cmd/golangci-lint@v1.61.0"
   "github.com/goreleaser/goreleaser/v2@latest"
   "github.com/bombsimon/wsl/v4/cmd/wsl@master"
   "github.com/sigstore/cosign/v2/cmd/cosign@latest"
